b06ee258594886d132c3f22a2a47bd296c696f5e
Shahriar MS <shahriar.ms1k@gmail.com>
2025-08-16 16:37:37 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -12,16 +12,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
-  webpack: (config, { isServer }) => {
-    // This is to make sure that the `node-thermal-printer` and `get-pixels` libraries,
-    // which are Node.js specific, are not included in the client-side bundle.
-    // They will only be bundled for the server-side, which is our API route.
-    if (!isServer) {
-        config.externals.push('node-thermal-printer');
-        config.externals.push('get-pixels');
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
